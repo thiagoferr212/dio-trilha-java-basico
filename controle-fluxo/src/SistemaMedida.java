0a1e3cd41c81a285d9d77d63593498375ca406b1
Thiago Ferreira <thiagoferr212@gmail.com>
2024-07-28 20:12:06 -0300

    Aprendendo a utilizar o for each

---
+++ controle-fluxo/src/SistemaMedida.java
@@ -0,0 +1,18 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        
+        String sigla = "T";
+
+        switch (sigla) {
+            case "T":
+                System.out.println("5Gb Youtube");                
+            case "M":
+                System.out.println("Whats e Instagram grátis");                
+            case "B":
+                System.out.println("100 minutis de ligação");                
+
+        }
+
+    }
+    
+}
