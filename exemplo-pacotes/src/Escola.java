7e339c9136dcc57af9bd7fc34ba5b46812510235
Thiago Ferreira <thiagoferr212@gmail.com>
2024-09-12 15:03:52 -0300

    fiz uma alteração do exercício anterior, para outro exercício de Getters e Setters.

---
+++ exemplo-pacotes/src/Escola.java
@@ -0,0 +1,9 @@
+public class Escola {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("joao");
+        aluno1.setIdade(15);
+
+        System.out.println("O aluno " + aluno1.getNome() + " tem " + aluno1.getIdade() + " anos.");
+    }
+}
